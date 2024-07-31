b94a622ee2ea7299180fb6914225895a2ef066de
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2024-07-31 11:23:48 +0000

    version bump script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,2 +1,16 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <value>"
+  exit 1
+fi
+
+VERSION_BUMP_TYPE=$1
+
+if [ "$VERSION_BUMP_TYPE" = "release" ]; then
+  pnpm exec changeset pre exit || true
+else
+  pnpm exec changeset pre enter rc || true
+fi
 pnpm exec changeset version
 pnpm install --lockfile-only
