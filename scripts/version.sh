e239f6b6cf54dc26768e24a0fee511bec6336a7b
Terashima Shin <sterashima78@gmail.com>
2024-07-31 22:56:42 +0000

    update script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <value>"
+  echo "Usage: $0 <release | pre-relase>"
   exit 1
 fi
 
 VERSION_BUMP_TYPE=$1
 
+# 要求されたモードに移行する
+# 既にそのモードの場合はエラーになるが後続の実行には影響しない
 if [ "$VERSION_BUMP_TYPE" = "release" ]; then
   pnpm exec changeset pre exit || true
 else
